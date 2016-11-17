e610837466fe86b7774815597b1fccfdb3ba7aa9
Carl Vitzthum <carl.vitzthum@gmail.com>
2016-11-17 10:14:32 -0500

    Added comments to setup.py and test.py; removed licensing info in a couple places. Removed unused files: tabixmodule.c and libcpairix.pyx

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
-# Adapted from pypairix and pysam
+# Written by Soo Lee and Carl Vitzthum
+# This code is based on the following open-source projects:
+# pytabix (https://github.com/slowkow/pytabix)
+# pysam (https://github.com/pysam-developers)
+# The Github repo for this project is:
+# https://github.com/4dn-dcic/pairix
+
 from setuptools import setup, find_packages, Extension
 
 EXT_MODULES = [
@@ -19,10 +25,10 @@ setup(
     version = "0.0.1",
     description = "Python interface for pairix",
     url = "https://github.com/4dn-dcic/pairix",
-    author = "...",
-    author_email = "...",
+    author = "Soo Lee",
+    author_email = "duplexa@gmail.com",
     license = "...",
-    keywords = ["pairix","pairix", "bgzip", "bioinformatics", "genomics","hi-c"],
+    keywords = ["pairix","tabix", "bgzip", "bioinformatics", "genomics","hi-c"],
     packages = find_packages(),
     package_data = { "": ["*.gz", "*.gz.px2"] },
     ext_modules = EXT_MODULES,
@@ -37,74 +43,5 @@ setup(
         "Programming Language :: C",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
-    ],
-    long_description = """\
-April 16, 2014
-
-This module allows fast random access to files compressed with bgzip_ and
-indexed by pairix_. It includes a C extension with code from klib_. The bgzip
-and pairix programs are available here_.
-
-
-Installation
-------------
-
-::
-
-    pip install --user pypairix
-
-
-Synopsis
---------
-
-Genomics data is often in a table where each row corresponds to a genomic
-region (start, end) or a position:
-
-
-::
-
-    chrom  pos      snp
-    1      1000760  rs75316104
-    1      1000894  rs114006445
-    1      1000910  rs79750022
-    1      1001177  rs4970401
-    1      1001256  rs78650406
-
-
-With pairix_, you can quickly retrieve all rows in a genomic region by
-specifying a query with a sequence name, start, and end:
-
-
-::
-
-    import pairix
-
-    # Open a remote or local file.
-    url = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20100804/"
-    url += "ALL.2of4intersection.20100804.genotypes.vcf.gz"
-
-    tb = pairix.open(url)
-
-    # These 1D queries are identical. A query returns an iterator over the results.
-    records = tb.query("1", 1000000, 1250000)
-    records = tb.queryi(0, 1000000, 1250000)
-    records = tb.querys("1:1000000-1250000")
-
-    # Each record is a list of strings.
-    for record in records:
-        print record[:5]
-        break
-
-
-::
-
-    ['1', '1000071', '.', 'C', 'T']
-
-
-.. _bgzip: http://samtools.sourceforge.net/pairix.shtml
-.. _pairix: http://samtools.sourceforge.net/pairix.shtml
-.. _klib: https://github.com/jmarshall/klib
-.. _here: http://sourceforge.net/projects/samtools/files/pairix/
-
-"""
+    ]
 )
