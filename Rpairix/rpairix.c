d3fe2f27e1f55a7a14015ec2c3443c5eaee884c0
SooLee <duplexa@gmail.com>
2016-12-09 01:08:24 -0500

    compilation symbol not found problem fixed. Added get_lines function. Works now.

--- Rpairix/rpairix.c
+++ Rpairix/rpairix.c
@@ -3,7 +3,7 @@
 // load
 tabix_t *load(char* fn){
 
-  tabix_t *tb; 
+  tabix_t *tb=NULL; 
 
   // index file name
   char fnidx[strlen(fn)+5];
@@ -21,17 +21,38 @@ tabix_t *load(char* fn){
 //load + get size of the query result
 void get_size(char** pfn, char** pquerystr, int* pn, int* pmax_len){
 
-   int len;
+   int len=-1;
    const char *s;
 
    tabix_t *tb = load(*pfn);
    const ti_conf_t *idxconf = ti_get_conf(tb->idx);
    ti_iter_t iter = ti_querys_2d(tb, *pquerystr);
 
+   *pn=0;
+   *pmax_len=0;
    while ((s = ti_read(tb, iter, &len)) != 0) {
-     if ((int)(*s) != idxconf->meta_char) break;   
+     // if ((int)(*s) != idxconf->meta_char) break;    // I don't fully understand this line. Works without the line.
      if(len>*pmax_len) *pmax_len = len;
      (*pn)++;
+   }
+
+}
+
+
+//load + return the result
+void get_lines(char** pfn, char** pquerystr, char** presultstr){
+
+   int len=-1;
+   const char *s;
+   int k=0;
+
+   tabix_t *tb = load(*pfn);
+   const ti_conf_t *idxconf = ti_get_conf(tb->idx);
+   ti_iter_t iter = ti_querys_2d(tb, *pquerystr);
+
+   while ((s = ti_read(tb, iter, &len)) != 0) {
+     // if ((int)(*s) != idxconf->meta_char) break;    // I don't fully understand this line. Works without the line.
+     strcpy(presultstr[k++],s);
    }
 
 }
