9368f89a51b223e292b9f6112886d49e57a0f3f8
SooLee <duplexa@gmail.com>
2016-12-12 00:40:58 -0500

    re-indexed sample files.

--- test/test_c.sh
+++ test/test_c.sh
@@ -1,7 +1,14 @@
+## 2D
 bin/pairix -f -s2 -d6 -b3 -e3 -u7 samples/merged_nodup.tab.chrblock_sorted.txt.gz
 bin/pairix samples/merged_nodup.tab.chrblock_sorted.txt.gz '10:1-1000000|20'
 bin/pairix samples/merged_nodup.tab.chrblock_sorted.txt.gz '10:1-1000000|20:50000000-60000000'
 bin/pairix samples/merged_nodup.tab.chrblock_sorted.txt.gz '1:1-10000000|20:50000000-60000000' '3:5000000-9000000|X:70000000-90000000'
+
+## 1D
 bin/pairix -s1 -b2 -e2 -f samples/SRR1171591.variants.snp.vqsr.p.vcf.gz
 bin/pairix samples/SRR1171591.variants.snp.vqsr.p.vcf.gz chr10:1-4000000
+
+## 2D, space-delimited
+bin/pairix -f -s2 -d6 -b3 -e3 -u7 -T samples/merged_nodups.space.chrblock_sorted.subsample1.txt.gz
+bin/pairix samples/merged_nodups.space.chrblock_sorted.subsample1.txt.gz '10:1-1000000|20'
 
