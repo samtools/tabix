a461963c32efdc375e2574c8ef58b32727e3d492
Soo Lee <duplexa@gmail.com>
2019-11-14 12:31:06 -0500

    Update sort-pairs.sh

--- util/sort-pairs.sh
+++ util/sort-pairs.sh
@@ -4,14 +4,14 @@ outprefix=$2
 
 mkfifo pp
 
+# compressing
+bgzip -c pp > $outprefix.pairs.gz &
+
 # header
 gunzip -c $f | grep "^#" > pp
     
 # sorting 
 gunzip -c $f | grep -v '^#' | sort -k2,2 -k4,4 -k3,3g -k5,5g >> pp
-    
-# compressing
-bgzip -c pp > $outprefix.pairs.gz
     
 # indexing
 pairix -f $outprefix.pairs.gz
